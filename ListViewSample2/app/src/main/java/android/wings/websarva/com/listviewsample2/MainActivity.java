2eedba7a44f39681320e4f23aa70af4089d4cf41
aryzae <a.aryzae@gmail.com>
2019-04-12 23:22:36 +0900

    Modify Chapter5.2.1

--- ListViewSample2/app/src/main/java/android/wings/websarva/com/listviewsample2/MainActivity.java
+++ ListViewSample2/app/src/main/java/android/wings/websarva/com/listviewsample2/MainActivity.java
@@ -2,6 +2,11 @@ package android.wings.websarva.com.listviewsample2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // ListViewオブジェクトを取得。
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        // ListViewに表示するリストデータ用オブジェクトを作成。
+        List<String> menuList = new ArrayList<>();
+        // リストデータの登録。
+        menuList.add("から揚げ定食");
+        menuList.add("ハンバーグ定食");
+        menuList.add("生姜焼き定食");
+        menuList.add("ステーキ定食");
+        menuList.add("野菜炒め定食");
+        menuList.add("とんかつ定食");
+        menuList.add("ミンチかつ定食");
+        menuList.add("チキンカツ定食");
+        menuList.add("コロッケ定食");
+        menuList.add("焼き魚定食");
+        menuList.add("焼肉定食");
+        // アダプタオブジェクトを生成。
+        // android.R.layout.simple_list_item_1は、デフォでOS側に用意されているListViewのCellのテンプレート
+        // android.Rクラスをインポートすると自動生成されるRクラスが読み込まれなくなるので、呼び出す時はandroid.Rで呼び出す
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, menuList);
+        // ListViewにアダプタオブジェクトを設定。
+        lvMenu.setAdapter(adapter);
     }
 }
