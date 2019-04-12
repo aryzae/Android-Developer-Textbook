9a4f8aee92d8e98432f171eac807f002b6d21a8c
aryzae <a.aryzae@gmail.com>
2019-04-12 17:15:41 +0900

    Modify Chapter5.1.2

--- ListViewSample/app/src/main/java/android/wings/websarva/com/listviewsample/MainActivity.java
+++ ListViewSample/app/src/main/java/android/wings/websarva/com/listviewsample/MainActivity.java
@@ -2,6 +2,12 @@ package android.wings.websarva.com.listviewsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import static android.widget.Toast.LENGTH_LONG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ListViewオブジェクトを取得。
+        ListView lvMenu = findViewById(R.id.lvMenu);
+        // ListViewにリストを設定。
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    /*
+     * リストがタップされたときの処理が記述されたメンバクラス
+     */
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            // タップされた定食名を取得。
+            String item = (String) parent.getItemAtPosition(position);
+            // トーストで表示する文字列を生成。
+            String show = "あなたが選んだ定食: " + item;
+            // トーストの表示。(Toast.LENGTH_LONGのToast.は省略可能)
+            Toast.makeText(MainActivity.this, show, Toast.LENGTH_LONG).show();
+        }
     }
 }
