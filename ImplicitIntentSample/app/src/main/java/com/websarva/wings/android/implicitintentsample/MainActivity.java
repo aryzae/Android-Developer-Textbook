314d78b3875e85fa1bbe715fccdc3399b5ec8fec
aryzae <a.aryzae@gmail.com>
2019-04-18 17:26:01 +0900

    Modify Chapter14.2.1

--- ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.java
+++ ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.java
@@ -13,6 +13,11 @@ import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 緯度
+    private double _latitude = 0;
+    // 軽度
+    private double _longitude = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,5 +43,16 @@ public class MainActivity extends AppCompatActivity {
         } catch (UnsupportedEncodingException e) {
             Log.e("IntentStartActivity", "検索キーワード変換失敗", e);
         }
+    }
+
+    public void onMapShowCurrentButtonClick(View view) {
+        // フィールドの緯度と軽度の値をもおとにマップアプリと連携するURI文字列を生成
+        String uriStr = "geo:" + _latitude + "," + _longitude;
+        // URI文字列からURIオブジェクトを生成
+        Uri uri = Uri.parse(uriStr);
+        // Intentオブジェクトを生成
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        // Activity開始
+        startActivity(intent);
     }
 }
