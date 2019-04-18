dd940745c60b965e49a82d4173307c509c46d442
aryzae <a.aryzae@gmail.com>
2019-04-18 17:18:43 +0900

    Modify Chapter14.1.3

--- ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.java
+++ ImplicitIntentSample/app/src/main/java/com/websarva/wings/android/implicitintentsample/MainActivity.java
@@ -1,7 +1,15 @@
 package com.websarva.wings.android.implicitintentsample;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onMapSearchButtonClick(View view) {
+        // 入力欄に入力されたキーワード文字列を取得
+        EditText etSearchWord = findViewById(R.id.etSearchWord);
+        String searchWord = etSearchWord.getText().toString();
+
+        try {
+            // 入力されたキーワードをURLエンコード
+            searchWord = URLEncoder.encode(searchWord, "UTF-8");
+            // マップアプリと連携するURI文字列を生成
+            String uriStr = "geo:0,0?q=" + searchWord;
+            // URI文字列からURIオブジェクトを生成
+            Uri uri = Uri.parse(uriStr);
+            // Intentオブジェクトを生成
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            // Activityを起動
+            startActivity(intent);
+        } catch (UnsupportedEncodingException e) {
+            Log.e("IntentStartActivity", "検索キーワード変換失敗", e);
+        }
     }
 }
