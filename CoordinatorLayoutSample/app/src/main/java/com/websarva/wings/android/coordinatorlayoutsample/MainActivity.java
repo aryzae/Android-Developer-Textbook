afa22b7061204192cf18ae8989cc64cb577c7b6e
aryzae <a.aryzae@gmail.com>
2019-04-20 14:58:41 +0900

    Modify Chapter16.4.1

--- CoordinatorLayoutSample/app/src/main/java/com/websarva/wings/android/coordinatorlayoutsample/MainActivity.java
+++ CoordinatorLayoutSample/app/src/main/java/com/websarva/wings/android/coordinatorlayoutsample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.websarva.wings.android.coordinatorlayoutsample;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Toolbarを取得
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // ツールバーにロゴ設定
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        // ツールバーにタイトル設定
+        toolbar.setTitle(R.string.toolbar_title);
+        // ツールバーにサブタイトル設定
+        toolbar.setSubtitle(R.string.toolbar_subtitle);
+        // ツールバーのサブタイトルの文字色設定
+        toolbar.setSubtitleTextColor(Color.LTGRAY);
+        // アクションバーにツールバーを設定
+        setSupportActionBar(toolbar);
     }
 }
